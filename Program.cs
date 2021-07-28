96f412d5d908623ee85736cf29e2b31e1a1ea9b7
AmruthaMallela <saiamruthamallela@gmail.com>
2021-07-28 09:57:35 +0530

    m

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            int a = 100;
+           
             int z = 50;
+            int y = 100;
         }
     }
 }
